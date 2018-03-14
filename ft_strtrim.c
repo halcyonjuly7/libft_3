d070c0edc2e4838d0f8a73fc050097227672fda9
Halcyon abrah Ramirez <hramirez@e1z2r4p34.42.us.org>
2018-03-13 18:41:19 -0700

    changes again

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,58 +6,37 @@
 /*   By: hramirez <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/27 14:52:17 by hramirez          #+#    #+#             */
-/*   Updated: 2018/03/13 17:28:59 by hramirez         ###   ########.fr       */
+/*   Updated: 2018/03/13 18:41:00 by hramirez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	get_start(char const *s)
+char	*ft_strtrim(char const *s)
 {
-	int		index;
+	unsigned int	i;
+	unsigned int	a;
+	unsigned int	b;
+	char			*str;
 
-	index = 0;
-	while ((s[index] == '\n' || s[index] == '\t' || s[index] == ' ')
-			&& s[index])
-		index++;
-	return (s[index] != '\0' ? index : -1);
-}
-
-static int	get_end(char const *s)
-{
-	int		str_len;
-
-	str_len = ft_strlen(s) - 1;
-	while ((s[str_len] == '\t' || s[str_len] == '\n' || s[str_len] == ' ')
-			&& str_len >= 0)
-		str_len--;
-	return (str_len);
-}
-
-#	define VARS int str_start,str_end, index; char *final_str
-
-char		*ft_strtrim(char const *s)
-{
-	VARS;
+	i = 0;
+	b = 0;
 	if (!s)
 		return (NULL);
-	index = 0;
-	str_start = get_start(s);
-	if (str_start == -1)
+	while (s[i] == ' ' || s[i] == '\n' || s[i] == '\t')
+		i++;
+	if (s[i] == '\0')
+		return (ft_strcpy(ft_memalloc(sizeof(char) * 2), ""));
+	a = ft_strlen(s) - 1;
+	while (s[a] == ' ' || s[a] == '\n' || s[a] == '\t')
+		a--;
+	if (!(str = (char *)malloc(sizeof(char) * (a - i + 2))))
+		return (NULL);
+	while (b < a - i + 1)
 	{
-		final_str = malloc(sizeof(char));
-		final_str[index] = '\0';
+		str[b] = s[i + b];
+		b++;
 	}
-	else
-	{
-		str_end = get_end(s);
-		index = 0;
-		final_str = malloc(sizeof(char) * (str_end - str_start) + 2);
-		if (final_str == NULL)
-			return (NULL);
-		while (str_start <= str_end)
-			final_str[index++] = s[str_start++];
-		final_str[index] = '\0';
-	}
-	return (final_str);
+	str[b] = '\0';
+	return (str);
 }
